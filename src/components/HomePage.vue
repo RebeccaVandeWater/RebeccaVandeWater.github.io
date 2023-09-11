4903af41f24fb11313049e3944ac2a8afd3b86b2
Becca <becca.vandewater@gmail.com>
2023-09-11 17:57:21 -0600

    animation!

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,8 +4,21 @@
       Hello!
     </p>
   </div>
-  <div class="flex justify-center">
-    Box
+  <div class="flex px-10 justify-between">
+    <div class="w-1/8">
+      Box
+    </div>
+    <div class="w-1/4 text-center">
+      <div class="hover:animate-spin">
+        Box
+      </div>
+    </div>
+    <div class="w-1/4">
+      Box
+    </div>
+    <div class="w-1/4">
+      Box
+    </div>
   </div>
 </template>
 
