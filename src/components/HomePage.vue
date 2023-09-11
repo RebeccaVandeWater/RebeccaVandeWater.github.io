db3ba918573aedf5f5e1b01c7c9f6be7ca077bde
Becca <becca.vandewater@gmail.com>
2023-09-11 17:45:51 -0600

    init

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-becca-blue hover:bg-cyan-700 transition-colors duration-1000 hover:text-white text-black">
+    <p>
+      Hello!
+    </p>
+  </div>
+  <div class="flex justify-center">
+    Box
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'becca-blue': '#FF006064'
+      }
+    },
+  },
+  plugins: [],
+}
+
