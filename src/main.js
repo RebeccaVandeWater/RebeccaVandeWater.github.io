db4d6e36e64ebbdc1e926d8e2cadcc6ed4086daa
Rebecca VandeWater <123334409+RebeccaVandeWater@users.noreply.github.com>
2023-09-19 14:00:49 -0600

    more github pages setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import HomePage from './pages/HomePage.vue'
 
-createApp(App).mount('#app')
+createApp(HomePage, App).mount('#app')
