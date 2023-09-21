e2637b29239aac060307ebaa9ede1e1970c114f0
Becca <becca.vandewater@gmail.com>
2023-09-21 13:14:20 -0600

    publish docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/landing-page/',
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    outDir: 'docs',
+    sourcemap: false
+  }
 })
